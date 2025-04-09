903d385dc1841451771f362eebccf6ce6911017d
2004Maycon <pequenosilvamaycon@gmail.com>
2025-04-09 08:12:46 -0300

    Update gerar matriz.py

--- gerar matriz.py
+++ gerar matriz.py
@@ -12,13 +12,13 @@ def print_matriz(criar_matriz):
     coluna = len(criar_matriz[0])
     for i in range(linha):
         for j in range(coluna):
-            print(criar_matriz[i][j],end=" ")
+            print(criar_matriz[i][j],end="")
         print()
 def main():
     quantidade_de_linhas = int(input("digite quantas linhas voce quer "))
     quantidade_coluna = int(input("quantas colunas "))
     min = int(input("qual parametro de numeros "))
     max = int(input("qual parametro de numeros "))
-    retornar_matriz  = matriz(quantidade_de_linhas,quantidade_coluna,min,max)
-    print_matriz(retornar_matriz)
+    retornar  = matriz(quantidade_de_linhas,quantidade_coluna,min,max)
+    print_matriz(retornar)
 main()
