c8a014ec9b8d32f74b460098248a87c18c532fef
2004Maycon <pequenosilvamaycon@gmail.com>
2025-04-09 10:15:33 -0300

    Create trocar_colunas.py

---
+++ trocar_colunas.py
@@ -0,0 +1,38 @@
+import random
+def matriz(linha,coluna,minimo,maximo):
+    criar_matriz= []
+    for i in range(linha):
+        list_coluna = []
+        for j in range(coluna):
+            list_coluna.append(random.randint(minimo,maximo))
+        criar_matriz.append(list_coluna)
+    return criar_matriz
+def trocar_coluna (criar_matriz):
+    coluna1 = []
+    coluna2 = []
+    aux = []
+    for i in range(len(criar_matriz)):
+        coluna1.append(criar_matriz[0])
+    for j in range(len(criar_matriz)):
+        coluna2.append(criar_matriz[3])
+    aux = coluna1
+    coluna1 = coluna2
+    criar_matriz.append(aux,coluna1)
+    return criar_matriz
+
+def print_matriz(criar_matriz):
+    linha = len(criar_matriz)
+    coluna = len(criar_matriz[0])
+    for i in range(linha):
+        for j in range(coluna):
+           print(criar_matriz[i][j],end="  ")
+        print()
+
+def main():
+    quantidade_de_linhas = int(input("digite quantas linhas voce quer "))
+    quantidade_coluna = int(input("quantas colunas "))
+    min = int(input("qual parametro de numeros "))
+    max = int(input("qual parametro de numeros "))
+    retornar  = matriz(quantidade_de_linhas,quantidade_coluna,min,max)
+    trocar_coluna(retornar)    
+main()
