1ef5629c0e62e164f9dee06f012edfca39a759d6
2004Maycon <pequenosilvamaycon@gmail.com>
2025-04-10 14:54:06 -0300

    Update trocar_colunas.py

--- trocar_colunas.py
+++ trocar_colunas.py
@@ -6,22 +6,8 @@ def matriz(linha,coluna,minimo,maximo):
         for j in range(coluna):
             list_coluna.append(random.randint(minimo,maximo))
         criar_matriz.append(list_coluna)
-    print(criar_matriz[i][j],end="")
-    print()
     return criar_matriz
-def trocar_coluna (criar_matriz):
-    col_A= []
-    col_b = []
-    aux = []
-    linha = len(criar_matriz)
-    coluna = len(criar_matriz[0])
-    if coluna > 2 :
-        for i in range(linha):
-            aux.append(criar_matriz[i][0])
-            criar_matriz[i][0] = criar_matriz[i][3]
-            criar_matriz[i][3] = aux[i]
-        return criar_matriz
-        print()
+
 def print_matriz(criar_matriz):
     linha = len(criar_matriz)
     coluna = len(criar_matriz[0])
@@ -30,13 +16,32 @@ def print_matriz(criar_matriz):
            print(criar_matriz[i][j],end="  ")
         print()
 
+def trocar_coluna (m,c1,c2):
+    for i in range(len(m)):
+        aux = m[i][c1]
+        m[i][c1] = m[i][c2]
+        m[i][c2] = aux
+    return m
+
+def print_coluna_trocada(m):
+    linha = len(m)
+    coluna = len(m[0])
+    for i in range(linha):
+        for j in range(coluna):
+           print(m[i][j],end="  ")
+        print()
+
 def main():
     quantidade_de_linhas = int(input("digite quantas linhas voce quer "))
     quantidade_coluna = int(input("quantas colunas "))
     min = int(input("qual parametro de numeros "))
     max = int(input("qual parametro de numeros "))
+    col1= int(input("qual a 1 coluna?"))
+    col2= int(input("qual a 1 coluna?"))
     retornar  = matriz(quantidade_de_linhas,quantidade_coluna,min,max)
-    coluna_trocada =trocar_coluna(retornar)
-    print_matriz(coluna_trocada)
-    print(f"coluna trocada{print_matriz}")
+    print_matriz(retornar)
+    print()
+    coluna_trocada =trocar_coluna(retornar,col1,col2)
+    print_coluna_trocada(coluna_trocada)
+    print()
 main()
